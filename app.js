8d57414692668fbefe31f5e27ada02fcc8c0b053
John Siordia <siohn@proton.me>
2024-10-03 22:10:00 -0500

    file rename, begin routes/controllers

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
+
+const app = express();
 const { PORT = 3001 } = process.env;
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+app.use("/", mainRouter);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch(console.error);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
